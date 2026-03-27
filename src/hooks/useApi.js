c89bca4e3894496005e2e2e05e8e022457296811
Kadambari Miraje <kadambarimirje04@gmail.com>
2026-03-27 11:24:58 +0530

    added logo

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_API_URL || 'https://65-2-189-227.nip.io/api';
-// const BASE_URL = import.meta.env.VITE_API_URL || 'http://192.168.0.163:8083/api';
+
 
 const api = axios.create({ baseURL: BASE_URL });
 
