0a80d734a8629fbb7e038b969dde994ecf2dffd0
Kadambari Miraje <kadambarimirje04@gmail.com>
2026-04-02 18:04:40 +0530

    Changes done in api urls

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const BASE_URL = import.meta.env.VITE_API_URL || 'https://65-2-189-227.nip.io/api';
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://10.29.70.131:8083/api';
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://65-2-189-227.nip.io/api';
+// const BASE_URL = import.meta.env.VITE_API_URL || 'http://10.29.70.131:8083/api';
 
 const api = axios.create({ baseURL: BASE_URL });
 
